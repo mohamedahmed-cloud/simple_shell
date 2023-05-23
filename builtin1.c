b6b97fbe20f00a85c3bc87ef3dbeeb862df9af19
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-05-23 18:10:39 +0300

    Fix The Problem

--- builtin1.c
+++ builtin1.c
@@ -3,99 +3,59 @@
 /**
  * _myhistory - displays the history list, one command by line, preceded
  *              with line numbers, starting at 0.
- * @Inf: Structure containing potential arguments. Used to maintain
+ * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
-int _myhistory(info_t *Inf)
+int _myhistory(info_t *info)
 {
-	print_list(Inf->history);
+	print_list(info->history);
 	return (0);
 }
 
 /**
- * set_alias - sets alias to string
- * @Inf : parameter struct
- * @string: the string alias
- *
- * Return: Always 0 on success, 1 on error
- */
-int set_alias(info_t *Inf, char *string)
-{
-	char *ptr;
-
-	ptr = _strchr(string, '=');
-	if (!ptr)
-		return (1);
-	if (!*++ptr)
-		return (unset_alias(Inf, string));
-
-	unset_alias(Inf, string);
-	return (add_node_end(&(Inf->alias), string, 0) == NULL);
-}
-
-
-
-
-/**
  * unset_alias - sets alias to string
- * @Inf: parameter struct
- * @string: the string alias
+ * @info: parameter struct
+ * @str: the string alias
  *
  * Return: Always 0 on success, 1 on error
  */
-int unset_alias(info_t *Inf, char *string)
+int unset_alias(info_t *info, char *str)
 {
 	char *p, c;
 	int ret;
 
-	p = _strchr(string, '=');
+	p = _strchr(str, '=');
 	if (!p)
 		return (1);
 	c = *p;
 	*p = 0;
-	ret = delete_node_at_index(&(Inf->alias),
-		get_node_index(Inf->alias, node_starts_with(Inf->alias, string, -1)));
+	ret = delete_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
 	*p = c;
 	return (ret);
 }
 
-
 /**
- * _myalias - mimics the alias builtin (man alias)
- * @Inf: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * set_alias - sets alias to string
+ * @info: parameter struct
+ * @str: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
  */
-int _myalias(info_t *Inf)
+int set_alias(info_t *info, char *str)
 {
-	int i = 0;
-	char *ptr = NULL;
-	list_t *node = NULL;
+	char *p;
 
-	if (Inf->argc == 1)
-	{
-		node = Inf->alias;
-		while (node)
-		{
-			print_alias(node);
-			node = node->next;
-		}
-		return (0);
-	}
-	for (i = 1; Inf->argv[i]; i++)
-	{
-		ptr = _strchr(Inf->argv[i], '=');
-		if (ptr)
-			set_alias(Inf, Inf->argv[i]);
-		else
-			print_alias(node_starts_with(Inf->alias, Inf->argv[i], '='));
-	}
+	p = _strchr(str, '=');
+	if (!p)
+		return (1);
+	if (!*++p)
+		return (unset_alias(info, str));
 
-	return (0);
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
-
-
 
 /**
  * print_alias - prints an alias string
@@ -105,18 +65,51 @@ int _myalias(info_t *Inf)
  */
 int print_alias(list_t *node)
 {
-	char *ptr = NULL, *a = NULL;
+	char *p = NULL, *a = NULL;
 
 	if (node)
 	{
-		ptr = _strchr(node->str, '=');
-		for (a = node->str; a <= ptr; a++)
+		p = _strchr(node->str, '=');
+		for (a = node->str; a <= p; a++)
 		_putchar(*a);
 		_putchar('\'');
-		_puts(ptr + 1);
+		_puts(p + 1);
 		_puts("'\n");
 		return (0);
 	}
 	return (1);
 }
 
+/**
+ * _myalias - mimics the alias builtin (man alias)
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+	int i = 0;
+	char *p = NULL;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		p = _strchr(info->argv[i], '=');
+		if (p)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}
--- getLine.c
+++ getLine.c
@@ -1,16 +1,125 @@
 #include "shell.h"
 
 /**
+ * input_buf - buffers chained commands
+ * @info: parameter struct
+ * @buf: address of buffer
+ * @len: address of len var
+ *
+ * Return: bytes read
+ */
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len) /* if nothing left in the buffer, fill it */
+	{
+		/*bfree((void **)info->cmd_buf);*/
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			/* if (_strchr(*buf, ';')) is this a command chain? */
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+
+/**
+ * get_input - gets a line minus the newline
+ * @info: parameter struct
+ *
+ * Return: bytes read
+ */
+ssize_t get_input(info_t *info)
+{
+	static char *buf; /* the ';' command chain buffer */
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *p;
+
+	_putchar(BUF_FLUSH);
+	r = input_buf(info, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len) /* we have commands left in the chain buffer */
+	{
+		j = i; /* init new iterator to current buf position */
+		p = buf + i; /* get pointer for return */
+
+		check_chain(info, buf, &j, i, len);
+		while (j < len) /* iterate to semicolon or end */
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
+
+		i = j + 1; /* increment past nulled ';'' */
+		if (i >= len) /* reached end of buffer? */
+		{
+			i = len = 0; /* reset position and length */
+			info->cmd_buf_type = CMD_NORM;
+		}
+
+		*buf_p = p; /* pass back pointer to current command position */
+		return (_strlen(p)); /* return length of current command */
+	}
+
+	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
+	return (r); /* return length of buffer from _getline() */
+}
+
+/**
+ * read_buf - reads a buffer
+ * @info: parameter struct
+ * @buf: buffer
+ * @i: size
+ *
+ * Return: r
+ */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+
+/**
  * _getline - gets the next line of input from STDIN
- * @Inf: parameter struct
- * @length: size of preallocated ptr buffer if not NULL
+ * @info: parameter struct
  * @ptr: address of pointer to buffer, preallocated or NULL
+ * @length: size of preallocated ptr buffer if not NULL
  *
  * Return: s
  */
-int _getline(info_t *Inf, char **ptr, size_t *length)
+int _getline(info_t *info, char **ptr, size_t *length)
 {
-	static char buffer[READ_BUF_SIZE];
+	static char buf[READ_BUF_SIZE];
 	static size_t i, len;
 	size_t k;
 	ssize_t r = 0, s = 0;
@@ -22,20 +131,20 @@ int _getline(info_t *Inf, char **ptr, size_t *length)
 	if (i == len)
 		i = len = 0;
 
-	r = read_buf(Inf, buffer, &len);
+	r = read_buf(info, buf, &len);
 	if (r == -1 || (r == 0 && len == 0))
 		return (-1);
 
-	c = _strchr(buffer + i, '\n');
-	k = c ? 1 + (unsigned int)(c - buffer) : len;
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(p, s, s ? s + k : k + 1);
 	if (!new_p) /* MALLOC FAILURE! */
 		return (p ? free(p), -1 : -1);
 
 	if (s)
-		_strncat(new_p, buffer + i, k - i);
+		_strncat(new_p, buf + i, k - i);
 	else
-		_strncpy(new_p, buffer + i, k - i + 1);
+		_strncpy(new_p, buf + i, k - i + 1);
 
 	s += k - i;
 	i = k;
@@ -47,126 +156,15 @@ int _getline(info_t *Inf, char **ptr, size_t *length)
 	return (s);
 }
 
-
-
-/**
- * get_input - gets a line minus the newline
- * @Inf: parameter struct
- *
- * Return: bytes read
- */
-ssize_t get_input(info_t *Inf)
-{
-	static char *buffer;
-	static size_t i, j, len;
-	ssize_t r = 0;
-	char **buf_p = &(Inf->arg), *p;
-
-	_putchar(BUF_FLUSH);
-	r = input_buf(Inf, &buffer, &len);
-	if (r == -1)
-		return (-1);
-	if (len)
-	{
-		j = i;
-		p = buffer + i;
-
-		check_chain(Inf, buffer, &j, i, len);
-		while (j < len)
-		{
-			if (is_chain(Inf, buffer, &j))
-				break;
-			j++;
-		}
-		i = j + 1;
-		if (i >= len)
-		{
-			i = len = 0;
-			Inf->cmd_buf_type = CMD_NORM;
-		}
-
-		*buf_p = p;
-		return (_strlen(p));
-	}
-
-	*buf_p = buffer;
-	return (r);
-}
-
-
-
-/**
- * input_buf - buffers chained commands
- * @buffer: address of buffer
- * @Inf: parameter struct
- * @addlen: address of len var
- *
- * Return: bytes read
- */
-ssize_t input_buf(info_t *Inf, char **buffer, size_t *addlen)
-{
-	ssize_t r = 0;
-	size_t len_p = 0;
-
-	if (!*addlen)
-	{
-		free(*buffer);
-		*buffer = NULL;
-		signal(SIGINT, sigintHandler);
-#if USE_GETLINE
-		r = getline(buf, &len_p, stdin);
-#else
-		r = _getline(Inf, buffer, &len_p);
-#endif
-		if (r > 0)
-		{
-			if ((*buffer)[r - 1] == '\n')
-			{
-				(*buffer)[r - 1] = '\0';
-				r--;
-			}
-			Inf->linecount_flag = 1;
-			remove_comments(*buffer);
-			build_history_list(Inf, *buffer, Inf->histcount++);
-			{
-				*addlen = r;
-				Inf->cmd_buf = buffer;
-			}
-		}
-	}
-	return (r);
-}
-
-
 /**
  * sigintHandler - blocks ctrl-C
- * @signal_num: the signal number
+ * @sig_num: the signal number
  *
  * Return: void
  */
-void sigintHandler(__attribute__((unused))int signal_num)
+void sigintHandler(__attribute__((unused))int sig_num)
 {
 	_puts("\n");
 	_puts("$ ");
 	_putchar(BUF_FLUSH);
-}
-
-/**
- * read_buf - reads a buffer
- * @Inf: parameter struct
- * @i: size
- * @buffer: buffer
- *
- * Return: r
- */
-ssize_t read_buf(info_t *Inf, char *buffer, size_t *i)
-{
-	ssize_t r = 0;
-
-	if (*i)
-		return (0);
-	r = read(Inf->readfd, buffer, READ_BUF_SIZE);
-	if (r >= 0)
-		*i = r;
-	return (r);
 }
